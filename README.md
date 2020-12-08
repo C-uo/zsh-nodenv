ecc9d8adf3d4fbee80a697191a5133622e51eb2d
Cuo <cuo.github@gmail.com>
2020-12-09 05:35:16 +0900

    support nodenv (#1)
    * Update README.md
    
    * Create zshenv.plugin.zsh
    
    https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/pyenv/pyenv.plugin.zsh
    
    * support nodenv
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # zsh-env
-plugin for nodenv &amp; goenv like [ohmyzsh/plugins/pyenv](https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/pyenv)
+plugin for nodenv & ~~goenv~~ like [ohmyzsh/plugins/pyenv](https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/pyenv)
+
+
+This plugin looks for [nodenv](https://github.com/nodenv/nodenv), and loads it if it's found.
+
+## Installtion
+### Using [Zinit](https://github.com/zdharma/zinit)
+Load `zsh-env` as a plugin in your `.zshrc`
+
+```zsh
+# Plugin zsh-env loaded with investigating.
+zinit load C-uo/zsh-env
+# Plugin zsh-env loaded without investigating.
+zinit light C-uo/zsh-env
+```
---
+++ nodenv.plugin.zsh
@@ -0,0 +1,34 @@
+# This plugin loads nodenv into the current shell 
+
+# Load pyenv only if command not already available
+if command -v nodenv &> /dev/null && [[ "$(uname -r)" != *icrosoft* ]]; then
+    FOUND_NODENV=1
+else
+    FOUND_NODENV=0
+fi
+
+if [[ $FOUND_NODENV -ne 1 ]]; then
+    nodenvdirs=("$HOME/.nodenv" "/usr/local/nodenv" "/opt/nodenv" "/usr/local/opt/nodenv")
+    for dir in $nodenvdirs; do
+        if [[ -d $dir/bin ]]; then
+            export PATH="$PATH:$dir/bin"
+            FOUND_NODENV=1
+            break
+        fi
+    done
+fi
+
+if [[ $FOUND_NODENV -ne 1 ]]; then
+    if (( $+commands[brew] )) && dir=$(brew --prefix nodenv 2>/dev/null); then
+        if [[ -d $dir/bin ]]; then
+            export PATH="$PATH:$dir/bin"
+            FOUND_NODENV=1
+        fi
+    fi
+fi
+
+if [[ $FOUND_NODENV -eq 1 ]]; then
+    eval "$(nodenv init - --no-rehash zsh)"
+fi
+
+unset FOUND_NODENV nodenvdirs dir
